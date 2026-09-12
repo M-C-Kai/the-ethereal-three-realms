08342d8335a254b0d13d7906be75abfe3649920b
Chenkai <58809786+M-C-Kai@users.noreply.github.com>
2026-09-12 17:11:13 +0800

    refactor: add battle package

---
+++ implementation_staging/battle/__init__.py
@@ -0,0 +1,13 @@
+from .state import (
+    CONTACT_RADIUS_TILES,
+    RETRIGGER_TIMEOUT_SECONDS,
+    CombatStats,
+    LocalBattleState,
+)
+
+__all__ = [
+    'CONTACT_RADIUS_TILES',
+    'RETRIGGER_TIMEOUT_SECONDS',
+    'CombatStats',
+    'LocalBattleState',
+]
